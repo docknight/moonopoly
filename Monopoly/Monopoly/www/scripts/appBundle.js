5eace9fd5ec4b878baf7cfb22bc0c159b7ef584c
Tomaz Zavrsnik <tzavrsnik@gmail.com>
2015-11-29 21:55:37 +0100

    added basic render loop with a Monopoly board;

--- Monopoly/Monopoly/www/scripts/appBundle.js
+++ Monopoly/Monopoly/www/scripts/appBundle.js
@@ -325,6 +325,7 @@ var MonopolyApp;
                 this.stateService = stateService;
                 this.gameService = gameService;
                 this.initGame();
+                this.createScene();
             }
             Object.defineProperty(GameController.prototype, "currentPlayer", {
                 get: function () {
@@ -335,6 +336,42 @@ var MonopolyApp;
             });
             GameController.prototype.initGame = function () {
                 this.gameService.initGame();
+            };
+            GameController.prototype.createScene = function () {
+                var canvas = document.getElementById("renderCanvas");
+                var engine = new BABYLON.Engine(canvas, true);
+                var scene = this.createBoard(engine, canvas);
+                engine.runRenderLoop(function () {
+                    scene.render();
+                });
+                window.addEventListener("resize", function () {
+                    engine.resize();
+                });
+                // Watch for browser/canvas resize events
+                window.addEventListener("resize", function () {
+                    engine.resize();
+                });
+            };
+            GameController.prototype.createBoard = function (engine, canvas) {
+                // This creates a basic Babylon Scene object (non-mesh)
+                var scene = new BABYLON.Scene(engine);
+                // This creates and positions a free camera (non-mesh)
+                var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+                // This targets the camera to scene origin
+                camera.setTarget(BABYLON.Vector3.Zero());
+                // This attaches the camera to the canvas
+                camera.attachControl(canvas, true);
+                // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+                var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+                // Default intensity is 1. Let's dim the light a small amount
+                light.intensity = 1;
+                // Our built-in 'ground' shape. Params: name, width, depth, subdivs, scene
+                var board = BABYLON.Mesh.CreateGround("ground1", 6, 6, 2, scene);
+                var boardMaterial = new BABYLON.StandardMaterial("boardTexture", scene);
+                boardMaterial.emissiveTexture = new BABYLON.Texture("images/Gameboard.png", scene);
+                boardMaterial.diffuseTexture = new BABYLON.Texture("images/Gameboard.png", scene);
+                board.material = boardMaterial;
+                return scene;
             };
             GameController.$inject = ["$state", "gameService"];
             return GameController;
